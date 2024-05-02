71b00a9cf329d9540b5079cc0fd6d35030abcef0
alaa wahba <alaawahbaa13@gmail.com>
2024-05-02 20:47:35 +0300

    Adding Relations

--- APIs/src/models/index.models.js
+++ APIs/src/models/index.models.js
@@ -26,46 +26,75 @@ const TreatmentPlans = TreatmentModel(db, Sequelize);
 const Radiography = RadiographyModel(db, Sequelize);
 const MedicalAnalysis = MedicalAnaylsisModel(db, Sequelize);
 
-//models for many -> many
-const PatientReservedBeds = db.define("patient_reservedBeds")
-const CycleTreatmentPlans = db.define("cycle_treatmentPlans")
-const defaultCycles = db.define("cycle_treatmentPlanReadOnly")
-const CyclePremedications = db.define("cycle_premedications")
-const CycleChemotherapy = db.define("cycle_chemotherapy")
-const defaultPremedications = db.define("premedications_treatmentPlanReadOnly")
-const PlansPremedications = db.define("premedications_treatmentPlans")
+// models for many -> many
+const PatientReservedBeds = db.define("patient_reservedBeds");
+const CycleTreatmentPlans = db.define("cycle_treatmentPlans");
+const defaultCycles = db.define("cycle_treatmentPlanReadOnly");
+const CyclePremedications = db.define("cycle_premedications");
+const CycleChemotherapy = db.define("cycle_chemotherapy");
+const defaultPremedications = db.define("premedications_treatmentPlanReadOnly");
+const PlansPremedications = db.define("premedications_treatmentPlans");
 
+// ====================M to M Relations===================
+//1. patients && reserved beds
+Patients.belongsToMany(ReservedBeds, { through: PatientReservedBeds });
+ReservedBeds.belongsToMany(Patients, { through: PatientReservedBeds });
 
+//2. Treatment plans & cycles
+Cycles.belongsToMany(TreatmentPlans, { through: CycleTreatmentPlans });
+TreatmentPlans.belongsToMany(Cycles, { through: CycleTreatmentPlans });
 
-// define relationships
-// Patients & ReservedBeds (many -> many)
-Patients.belongsToMany(ReservedBeds, { through: PatientReservedBeds })
-ReservedBeds.belongsToMany(Patients, { through: PatientReservedBeds })
+//3.Treatment plans read only & cycles
+Cycles.belongsToMany(treatmentPlanReadOnly, { through: defaultCycles });
+treatmentPlanReadOnly.belongsToMany(Cycles, { through: defaultCycles });
 
-// TreatmentPlans & Cycles (many -> many)
-Cycles.belongsToMany(TreatmentPlans, { through: CycleTreatmentPlans })
-TreatmentPlans.belongsToMany(Cycles, { through: CycleTreatmentPlans })
+//4. cycles & premedications
+Cycles.belongsToMany(Premedications, { through: CyclePremedications });
+Premedications.belongsToMany(Cycles, { through: CyclePremedications });
+//5. cycles & chemotherapy medications
+Cycles.belongsToMany(ChemotherapyMedications, { through: CycleChemotherapy });
+ChemotherapyMedications.belongsToMany(Cycles, { through: CycleChemotherapy });
 
-// treatmentPlanReadOnly & Cycles (many -> many)
-Cycles.belongsToMany(treatmentPlanReadOnly, { through: defaultCycles })
-treatmentPlanReadOnly.belongsToMany(Cycles, { through:defaultCycles })
+//6. premedications & Treatment plans read only
+Premedications.belongsToMany(treatmentPlanReadOnly, {
+  through: defaultPremedications,
+});
+treatmentPlanReadOnly.belongsToMany(Premedications, {
+  through: defaultPremedications,
+});
+//7. premedications & Treatment plans
+Premedications.belongsToMany(TreatmentPlans, { through: PlansPremedications });
+TreatmentPlans.belongsToMany(Premedications, { through: PlansPremedications });
 
-// Premedications & Cycles (many -> many)
-Cycles.belongsToMany(Premedications, { through: CyclePremedications })
-Premedications.belongsToMany(Cycles, { through: CyclePremedications })
-
-// ChemotherapyMedications & Cycles (many -> many)
-Cycles.belongsToMany(ChemotherapyMedications, { through: CycleChemotherapy })
-ChemotherapyMedications.belongsToMany(Cycles, { through: CycleChemotherapy })
-
-// Premedications & treatmentPlanReadOnly(many -> many)
-Premedications.belongsToMany(treatmentPlanReadOnly, { through: defaultPremedications })
-treatmentPlanReadOnly.belongsToMany(Premedications, { through: defaultPremedications })
-
-// Premedications & treatmentPlanReadOnly(many -> many)
-Premedications.belongsToMany(TreatmentPlans, { through: PlansPremedications })
-TreatmentPlans.belongsToMany(Premedications, { through: PlansPremedications })
-
+//====================One to Many=======================
+//1. Cancer overview & treatment plans Read Only
+// CancerOverview.hasMany(treatmentPlanReadOnly);
+// treatmentPlanReadOnly.belongsTo(CancerOverview);
+//2. patients &Radiography
+Patients.hasMany(Radiography);
+Radiography.belongsTo(Patients);
+//3. patients& medical analysis
+Patients.hasMany(MedicalAnalysis);
+MedicalAnalysis.belongsTo(Patients);
+//====================One to One=======================
+//1. Vital signs & patients
+Patients.hasOne(VitalSign, {
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+VitalSign.belongsTo(Patients);
+//2. patients & cancer overview
+Patients.hasOne(CancerOverview, {
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+CancerOverview.belongsTo(Patients);
+//3. patients & treatment plans
+Patients.hasOne(TreatmentPlans, {
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+TreatmentPlans.belongsTo(Patients);
 
 // generate tables in DB
 
@@ -80,6 +109,5 @@ module.exports = {
   Premedications,
   TreatmentPlans,
   Radiography,
-  MedicalAnalysis
+  MedicalAnalysis,
 };
-
