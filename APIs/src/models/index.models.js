07ca098079c6369d721785c54bb80f6e759e5f58
Hager706 <hagertarek706@gmail.com>
2024-05-02 14:17:29 +0300

    many to many relations

--- APIs/src/models/index.models.js
+++ APIs/src/models/index.models.js
@@ -24,10 +24,47 @@ const  VitalSign =VitalSignModel(db,Sequelize)
 const  Premedications = PremedicationsModel(db,Sequelize)
 const  TreatmentPlans = TreatmentModel(db,Sequelize)
 
+//models for many -> many
+const PatientReservedBeds = db.define("patient_reservedBeds")
+const CycleTreatmentPlans = db.define("cycle_treatmentPlans")
+const defaultCycles = db.define("cycle_treatmentPlanReadOnly")
+const CyclePremedications = db.define("cycle_premedications")
+const CycleChemotherapy = db.define("cycle_chemotherapy")
+const defaultPremedications = db.define("premedications_treatmentPlanReadOnly")
+const PlansPremedications = db.define("premedications_treatmentPlans")
+
+
+// define relationships
+// Patients & ReservedBeds (many -> many)
+Patients.belongsToMany(ReservedBeds, { through: PatientReservedBeds })
+ReservedBeds.belongsToMany(Patients, { through: PatientReservedBeds })
+
+// TreatmentPlans & Cycles (many -> many)
+Cycles.belongsToMany(TreatmentPlans, { through: CycleTreatmentPlans })
+TreatmentPlans.belongsToMany(Cycles, { through: CycleTreatmentPlans })
+
+// treatmentPlanReadOnly & Cycles (many -> many)
+Cycles.belongsToMany(treatmentPlanReadOnly, { through: defaultCycles })
+treatmentPlanReadOnly.belongsToMany(Cycles, { through:defaultCycles })
+
+// Premedications & Cycles (many -> many)
+Cycles.belongsToMany(Premedications, { through: CyclePremedications })
+Premedications.belongsToMany(Cycles, { through: CyclePremedications })
+
+// ChemotherapyMedications & Cycles (many -> many)
+Cycles.belongsToMany(ChemotherapyMedications, { through: CycleChemotherapy })
+ChemotherapyMedications.belongsToMany(Cycles, { through: CycleChemotherapy })
+
+// Premedications & treatmentPlanReadOnly(many -> many)
+Premedications.belongsToMany(treatmentPlanReadOnly, { through: defaultPremedications })
+treatmentPlanReadOnly.belongsToMany(Premedications, { through: defaultPremedications })
+
+// Premedications & treatmentPlanReadOnly(many -> many)
+Premedications.belongsToMany(TreatmentPlans, { through: PlansPremedications })
+TreatmentPlans.belongsToMany(Premedications, { through: PlansPremedications })
+
 
 // generate tables in DB
-
-    
 module.exports={
       treatmentPlanReadOnly,
       Cycles,
