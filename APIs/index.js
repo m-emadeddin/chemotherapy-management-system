9227a4aca981920861ba3a03d73ef15a20e90672
alaa wahba <alaawahbaa13@gmail.com>
2024-05-04 01:47:53 +0300

    adding middleware

--- APIs/index.js
+++ APIs/index.js
@@ -1,53 +1,45 @@
 // const path = require('path');
-const PORT= process.env.PORT || 3000
-const express = require('express');
-const bodyParser = require('body-parser');
-const { insertData } = require('./src/models/index.models');
-
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const { insertData } = require("./src/models/index.models");
+const testRoutes = require("./src/routes/test");
 // import the DB
-const db =require ('./src/configs/db.config');
-const Dbc = require('./src/models/index.models')
+const db = require("./src/configs/db.config");
+const Dbc = require("./src/models/index.models");
 
 const app = express();
 
-
-// api routing 
+// api routing
 //const userRoute = require('./src/routes/users.routes')
 //app.use('/api/products', userRoute)
 
 //model imports
 //const User = require('./src/models/users.model');
 
+app.use(bodyParser.urlencoded({ extended: false })); //  do all the parsing of the body for us
+app.use(express.json()); // as we get all the api information on json format
 
-
-
-app.use(bodyParser.urlencoded({ extended: false })); //  do all the parsing of the body for us  
-app.use(express.json()) // as we get all the api information on json format 
-
-
-
-
-// set relation associations  
-
-
-// this part should be moved to src/models/index.models.js 
+// set relation associations
+app.use(testRoutes);
+// this part should be moved to src/models/index.models.js
 //conection to Db
 db.authenticate()
-.then(() => {
-    console.log('connected..')
+  .then(() => {
+    console.log("connected..");
     // Start the Express.js server
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error" + err);
   });
-})
-.catch(err => {
-    console.log('Error'+ err)
-})
 db.sync({ alter: true })
-.then(() => {
-  insertData()
-  console.log("Tables Created!")
-})
-.catch((err)=>{
-  console.log(err)
-})
+  .then(() => {
+    //insertData()
+    console.log("Tables Created!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
