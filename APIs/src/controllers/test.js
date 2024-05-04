c34628d66b4842719d8cd6bf6e601a43603cbd48
alaa wahba <alaawahbaa13@gmail.com>
2024-05-04 13:35:30 +0300

    adding chemotherapyMedication route

--- APIs/src/controllers/test.js
+++ APIs/src/controllers/test.js
@@ -27,14 +27,37 @@ exports.getChemotherapy = (req, res, next) => {
   const ID = req.params.id;
   Patients.findByPk(ID)
     .then((patient) => {
-      patient.getTreatmentPlan().then((treatmentplan) => {
-        info.cycle_count = treatmentplan.number_of_Cycles;
-        let plan_id = treatmentplan.Plan_ID;
-        treatmentplan.getCycles().then((cycles) => {
-            console.log(cycles);
+      return patient.getTreatmentPlan();
+    })
+    .then((treatmentplan) => {
+      info.cycle_count = treatmentplan.number_of_Cycles;
+      let plan_id = treatmentplan.Plan_ID;
+      return treatmentplan.getCycles();
+    })
+    .then((cycles) => {
+      const cyclePromises = cycles.map((cycle) => {
+        return cycle.getChemotherapyMedications().then((chemotherapy) => {
+          const chemoMeds = chemotherapy.map((chemoMeds) => ({
+            name: chemoMeds.Medication_Name,
+            dose: chemoMeds.Dose,
+            route : chemoMeds.Route,
+            Instructions: chemoMeds.Instructions,
+            Administered_Dose : chemoMeds.Administered_Dose_ml
+          }));
+          return {
+            cycleNumber: cycle.Cycle_Number,
+            chemotherapyMedications: chemoMeds,
+          };
         });
       });
+      return Promise.all(cyclePromises).then((cycleInfo) => {
+        const info = {
+          cycles: cycleInfo,
+        };
+        res.status(200).send(info);
+      });
     })
+
     .catch((err) => {
       console.log(err);
     });
