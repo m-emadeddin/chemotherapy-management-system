0e23d84f1979147f6e59e495e665e0912997bf49
AhmedAtef <aatef83156@gmail.com>
2024-05-06 19:34:54 +0300

    Test

--- client-side/src/pages/Patient/index.jsx
+++ client-side/src/pages/Patient/index.jsx
@@ -13,7 +13,7 @@ const data = [
   },
   {
     bilateralsalpi: "Excisional biopsy of left breats",
-    thirtyTwoThousandEighteen: "06/19/16 Requested",
+    thirtyTwoThousandEighteen: "06/19/19 Requested",
   },
 ];
 
