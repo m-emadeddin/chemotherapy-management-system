975163a2554a359f61dd9b6840228c10ce94ee37
Kamilia98 <kamiliaahmed01@gmail.com>
2024-05-07 06:54:07 +0300

    Implemented separate components for editing and reviewing, allowing to just toggle between them using buttons

---
+++ client-side/src/components/CycleDetails/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Text, Heading, Button } from "../../components";
+import { Link, useLocation } from "react-router-dom";
+import PremedicationsTable from "../../components/PremedicationsTable";
+import ChemotherapyTable from "../../components/ChemotherapyTable";
+
+const CycleDetails = () => {
+  console.log(2);
+  const location = useLocation();
+  const id = 1;
+  const activeCycle = 1;
+  const { cycle } = location.state || { cycle: activeCycle };
+  return (
+    <div className="flex flex-col gap-[20px]">
+      <div className="flex flex-col rounded-[20px] bg-white-A700 py-4">
+        <Text size="md" as="p" className="uppercase md:ml-0 p-[19px]">
+          Premedications
+        </Text>
+        <div className="h-px w-full self-stretch bg-blue_gray-100" />
+
+        <div>
+          <PremedicationsTable cycle={cycle} id={id}></PremedicationsTable>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-[20px] bg-white-A700 py-4">
+        <Text size="md" as="p" className=" uppercase md:ml-0 p-[19px]">
+          Chemotherapy
+        </Text>
+        <div>
+          <ChemotherapyTable cycle={cycle} id={id} />
+        </div>
+      </div>
+      <div className="flex flex-col p-[19px] gap-5">
+        <Text size="md" style={{ fontWeight: "bold" }}>
+          Physician notes
+        </Text>
+        <Text>Dose reduced Doxorubicin because patient has weak heart</Text>
+      </div>
+    </div>
+  );
+};
+export default CycleDetails;
