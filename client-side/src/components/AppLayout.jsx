125babd65a0594ae924c3d1ddc1815b51a9ecb9b
Kamilia98 <kamiliaahmed01@gmail.com>
2024-05-07 08:42:20 +0300

    Adjust spacing of App layout

--- client-side/src/components/AppLayout.jsx
+++ client-side/src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 function AppLayout() {
   return (
-    <div className="flex w-full flex-col items-center gap-[15px] bg-gray-100 ">
+    <div className="flex flex-1 w-full flex-col items-center gap-[15px] bg-gray-100 overflow-y-auto">
       {/* header section */}
       <Header className=" fixed  w-full top-0  flex items-center justify-center self-stretch border-b border-solid border-gray-400 bg-white-A700 p-2 shadow-xs z-50 " />
       <Outlet />
