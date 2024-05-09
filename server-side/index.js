d580913792a67711da447780bcc915164bd48126
alaa wahba <alaawahbaa13@gmail.com>
2024-05-10 01:33:13 +0300

    changing port to 5000

--- server-side/index.js
+++ server-side/index.js
@@ -1,13 +1,14 @@
 // const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const dummyData = require('./src/utils/data');
 
-
+//=============================== Routes ===============================================
 const PatientMedicationInfoRoutes = require("./src/routes/DocumentChemotherapy.routes");
 const UserRoute = require('./src/routes/SignPage.routes');
-// import the DB
+const OrderChemoRoute = require('./src/routes/OrderChemotherapy.routes');
+//=============================== Database ===============================================
 const db = require("./src/configs/db.config");
 const Dbc = require("./src/models/index.models");
 
@@ -16,11 +17,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); //  do all the parsing of the body for us
 app.use(express.json()); // as we get all the api information on json format
-// set relation associations
 
+//=============================== Middleware ===============================================
 app.use('/document-chemotherapy',PatientMedicationInfoRoutes);
 app.use('/users', UserRoute)
-
+app.use('/order',OrderChemoRoute);
 
 //conection to Db
 db.authenticate()
