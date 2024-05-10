1be35b6cb1334aa458cd7c92d64228d713f3560a
roaa gawish <roaa.ayman.mail@gmail.com>
2024-05-10 19:21:43 +0300

    update select-patient-api

--- server-side/src/controllers/SelectPatient.controllers.js
+++ server-side/src/controllers/SelectPatient.controllers.js
@@ -2,33 +2,13 @@ const { where } = require("sequelize");
 
 const Patients = require("../models/index.models").Patients;
 
-exports.getAllPatients = (req,res) => {
-    let info = Patients.findAll();
-    res.status(200).json({info}); 
-    
+exports.getAllPatients = (req, res) => {
+    Patients.findAll()
+        .then(patients => {
+            res.status(200).json({ patients });
+        })
+        .catch(error => {
+            console.error("Error fetching patients:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        });
 };
-
-// exports.getAllPatients = async (req, res) => {
-//     try {
-//         // Fetch all patients
-//         const patients = await Patients.findAll();
-
-//         // Send JSON response
-//         res.status(200).json({ patients });
-//     } catch (error) {
-//         // Handle errors
-//         console.error("Error fetching patients:", error);
-//         res.status(500).json({ error: "Internal Server Error" });
-//     }
-// };
-
-// exports.getAllPatients = (req,res) => {
-//     let info = Patients.findAll()
-//     .then(() => {
-//         res.status(200).send(info); 
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//       });
-    
-// }
