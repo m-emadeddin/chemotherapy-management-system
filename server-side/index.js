b0714f6b1c2480f70d8c5d9a97db2aa284fcf9b2
Hager706 <hagertarek706@gmail.com>
2024-05-10 17:16:01 +0300

    api for review page

--- server-side/index.js
+++ server-side/index.js
@@ -7,6 +7,8 @@ const dummyData = require('./src/utils/data');
 
 const PatientMedicationInfoRoutes = require("./src/routes/DocumentChemotherapy.routes");
 const UserRoute = require('./src/routes/SignPage.routes');
+const reviewChemotherabyRouter = require("./src/routes/ReviewChemotherapy.routes");
+
 // import the DB
 const db = require("./src/configs/db.config");
 const Dbc = require("./src/models/index.models");
@@ -20,6 +22,8 @@ app.use(express.json()); // as we get all the api information on json format
 
 app.use('/document-chemotherapy',PatientMedicationInfoRoutes);
 app.use('/users', UserRoute)
+app.use("/review-chemotherapy", reviewChemotherabyRouter);
+
 
 
 //conection to Db
@@ -45,3 +49,40 @@ dummyData.insertRegimens();
 .catch((err)=>{
   console.log(err)
 })
+//http://localhost:3000/review-chemotherapy/1
+/*{
+    "Plan_Name": "Lymphoma",
+    "number_of_Weeks": 3,
+    "number_of_Cycles": 5,
+    "PreMedications": [
+        {
+            "Medication_Name": "Predn",
+            "Dose": 100,
+            "Route": "Oral",
+            "Instructions": "Daily x 5 days. 1st dose 60 minutes prior to chemotherapy"
+        },
+        {
+            "Medication_Name": "Dox",
+            "Dose": 50,
+            "Route": "IV Push",
+            "Instructions": "IV Push over 15 minutes"
+        }
+    ],
+    "ChemotherapyMedications": [
+        {
+            "Medication_Name": "Pr",
+            "Dose": 100,
+            "Route": "Oral",
+            "Instructions": "Daily x 5 days. 1st dose 60 minutes prior to chemotherapy",
+            "Dosage_Reduction": null
+        },
+        {
+            "Medication_Name": "Dox",
+            "Dose": 50,
+            "Route": "Oral",
+            "Instructions": "IV Push over 15 minutes",
+            "Dosage_Reduction": "-10"
+        }
+    ],
+    "cycle_note": "Patient is allergic to penicillin."
+} */
