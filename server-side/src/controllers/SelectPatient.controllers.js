49994b5f855218374b6064111757083b781c1af3
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2024-05-12 09:25:22 +0300

    deleting patient

--- server-side/src/controllers/SelectPatient.controllers.js
+++ server-side/src/controllers/SelectPatient.controllers.js
@@ -12,3 +12,36 @@ exports.getAllPatients = (req, res) => {
             res.status(500).json({ error: "Internal Server Error" });
         });
 };
+
+
+exports.deletePatient = (req, res, next) => {
+    const patientId = req.params.id;
+    let Patient_Name;
+    Patients.findByPk(patientId)
+        .then((patient) => {
+            if (!patient) {
+                return res.status(404).json({ error: "Patient not found" });
+            }
+            Patient_Name = patient.Name;
+            Patients.destroy({
+                where: {
+                    Patient_ID: patientId
+                }
+            })
+            .then(deletedRows => {
+                console.log(deletedRows)
+                if (deletedRows === 0) {
+                    return res.status(404).json({ error: "Patient not found" });
+                }
+                res.status(200).json({ message: `Patient ${Patient_Name} deleted successfully` });
+            })
+            .catch(error => {
+                console.error("Error deleting patient:", error);
+                res.status(500).json({ error: "Internal Server Error" });
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching patient:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+    });
+};
