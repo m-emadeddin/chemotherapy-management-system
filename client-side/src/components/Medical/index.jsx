c3c1c54d2ed62c9b7c74ee0dd4f4a73162cb0fba
AhmedAtef <aatef83156@gmail.com>
2024-05-15 00:52:04 +0300

    edit date

--- client-side/src/components/Medical/index.jsx
+++ client-side/src/components/Medical/index.jsx
@@ -8,7 +8,7 @@ const MedicalAnalysisComponent = ({ medicalData }) => {
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const day = date.getDate().toString().padStart(2, "0");
   
-    return `${year}/${month}/${day}`;
+    return `${day}/${month}/${year}`;
   };
   return (
     <>
