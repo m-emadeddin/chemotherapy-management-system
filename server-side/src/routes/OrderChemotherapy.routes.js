279baa084585654c6614bc7e3b74adbf335aa100
AhmedAtef <aatef83156@gmail.com>
2024-05-15 02:53:26 +0300

    Fix 3shan Halimo

--- server-side/src/routes/OrderChemotherapy.routes.js
+++ server-side/src/routes/OrderChemotherapy.routes.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const orderChemo = require("../controllers/OrderChemotherapy.controllers");
+const orderChemo = require('../controllers/OrderChemotherapy.controllers');
 
-router.get("/get-regimen/:id", orderChemo.getRegimens);
-router.get("/pre-medications/:id", orderChemo.getPreMedications);
-router.get("/chemo-medications/:id", orderChemo.getChemoMedications);
-router.get("/patient-no/:id", orderChemo.cycleDate);
+router.get('/get-regimen/:id', orderChemo.getRegimens);
+router.get('/pre-medications/:id', orderChemo.getPreMedications);
+router.get('/chemo-medications/:id', orderChemo.getChemoMedications);
+router.get('/patient-no/:id', orderChemo.cycleDate);
 
 module.exports = router;
