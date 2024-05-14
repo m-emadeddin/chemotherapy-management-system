17dfb8d5d5a66da215425c69d96823547abc860a
Mohamed Abdull-haleam <76539072+Mohamedabdullhaleam@users.noreply.github.com>
2024-05-15 00:51:03 +0300

    Delete server-side/src/routes/SelectPatient.routes.js

--- server-side/src/routes/SelectPatient.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Patient = require('../controllers/SelectPatient.controllers');
-
-router.get('/all-patients',Patient.getAllPatients);
-router.delete("/delete-patient/:id", Patient.deletePatient);
-
-module.exports = router;
