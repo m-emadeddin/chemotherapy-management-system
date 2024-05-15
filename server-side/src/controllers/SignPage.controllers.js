adc974d4450e0b829a99fe16eb8ddbe751bd82d9
Hager706 <hagertarek706@gmail.com>
2024-05-15 21:17:45 +0300

    some changes

--- server-side/src/controllers/SignPage.controllers.js
+++ server-side/src/controllers/SignPage.controllers.js
@@ -1,131 +1,90 @@
-const bcrypt = require('bcrypt');
-const Users = require('../models/index.models').User;
-const { Op } = require('sequelize');
-const jwt = require('jsonwebtoken');
-const jwtSecret = process.env.JWT_SECRET;
+const db = require("../models/index.models");
 
-const authMiddleware = require("../middlewares/auth.middlewares");
-
-// Function to handle user registration (sign up)
-exports.UserSignUp = (req, res) => {
-    const { username, email, password } = req.body;
-    console.log(username)
-    // Validate email format
-    if (!validateEmail(email)) {
-        return res.status(400).json({ error: 'Invalid email format' });
+exports.reviewChemotheraby = async (req, res, next) => {
+  try {
+    // Define patient id
+    const { patientId } = req.params;
+    const patient = await db.Patients.findByPk(patientId);
+    if (!patient) {
+      return res.status(404).json({ error: "Patient not found" });
     }
 
-    // Check if the username or email already exists
-    Users.findOne({
-        where: {
-            [Op.or]: [
-                { Username: username },
-                { Email: email }
-            ]
-        }
-    }).then(existingUser => {
-        if (existingUser) {
-            return res.status(400).json({ error: 'Username or email already exists' });
-        }
-
-        // Hash the password securely
-        bcrypt.hash(password, 12)
-        .then(hashedPassword => {
-            // Create a new user record
-            Users.create({
-                Username: username,
-                Email: email,
-                Password: hashedPassword
-            })
-            .then(newUser => {
-                // Respond with success message
-                res.status(200).json({ message: 'User signed up successfully' });
-            })
-            .catch(error => {
-                console.error('Error creating user:', error);
-                res.status(500).json({ error: 'Internal server error' });
-            });
-        })
-        .catch(error => {
-            console.error('Error hashing password:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        });
-    })
-    .catch(error => {
-        console.error('Error finding existing user:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    const {
+      Plan_Name,
+      number_of_Weeks,
+      number_of_Cycles,
+      PreMedications,
+      ChemotherapyMedications,
+      Start_Date,
+    } = req.body;
+    // create a new Treatment Plan
+    const treatmentPlan = await patient.createTreatmentPlan({
+      Plan_Name,
+      number_of_Weeks,
+      number_of_Cycles,
     });
+    // define number of days for one cycle
+    let days = Math.floor((number_of_Weeks * 7) / number_of_Cycles) || 1;
+    // define start date for cycle
+    function addDays(date, days) {
+      const resultDate = new Date(date);
+      resultDate.setDate(resultDate.getDate() + days);
+      return resultDate;
+    }
+    // Create Cycles
+    let Cycles = await Promise.all(
+      [...new Array(number_of_Cycles)].map( async function (_, index) {
+      let cycle =   await db.Cycles.create({
+        Cycle_Number: index + 1,
+        Is_active: index === 0 ? true : false,
+        Start_Date:addDays(Start_Date, index * days),
+        Start_Time: "08:00:00",
+        End_Time: "17:00:00",
+      });
+      return cycle;
+    }))
+    // Create Premedications
+    const premedications = await Promise.all(
+      PreMedications.map(async (premedication) =>{
+      let preMedication = await db.Premedications.create({
+        Medication_Name: premedication.Medication_Name,
+        Dose: premedication.Dose,
+        Route: premedication.Route,
+        Instructions: premedication.Instructions,
+      });
+      return preMedication
+    }))
+    // Create Chemotherapy Medications  
+    const chemotherapyMedications = await Promise.all(ChemotherapyMedications.map(async (chemotherapymedication) =>{
+      let ChemotherapyMedication = await db.ChemotherapyMedications.create({
+        Medication_Name: chemotherapymedication.Medication_Name,
+        Dose: chemotherapymedication.Dose,
+        Route: chemotherapymedication.Route,
+        Instructions: chemotherapymedication.Instructions,
+        Dosage_Reduction: chemotherapymedication.Dosage_Reduction,
+      });
+      return ChemotherapyMedication
+    }))
+    // add cycles to treatment plane
+    await treatmentPlan.addCycles(Cycles)
+
+    // add premedications to cycles
+    await Promise.all(
+      Cycles.map(async (cycle) => {
+        return await cycle.addPremedications(premedications);
+      })
+    );
+
+    // add chemotherapy medications to cycles
+    await Promise.all(
+      Cycles.map(async (cycle) => {
+        return await cycle.addChemotherapyMedications(chemotherapyMedications);
+      })
+    );
+    // send response 
+    res.json({ message: "Data inserted successfully" });
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-
-// Function to handle user login (sign in)
-exports.UserSignIn = (req, res) => {
-    const { identifier, password } = req.body;
-
-    // Find the user by username or email
-    Users.findOne({ where: { 
-        [Op.or]: [
-            { Username: identifier },
-            { Email: identifier }
-        ]
-    }})
-    .then(user => {
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid username/email or password' });
-        }
-
-        // Check if the password matches
-        bcrypt.compare(password, user.Password)
-        .then(passwordMatch => {
-            if (!passwordMatch) {
-                return res.status(401).json({ error: 'Invalid username/email or password' });
-            }
-            const userId = user.user_ID;
-            console.log(userId)
-            // Generate JWT token
-            const token = jwt.sign({ userId: userId }, jwtSecret, { expiresIn: '1h' });
-
-            // Respond with success message and token
-            res.status(200).json({ message: 'Login successful', token });
-        }).catch(error => {
-            console.error('Error comparing passwords:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        });
-    }).catch(error => {
-        console.error('Error finding user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    });
-};
-
-exports.GetUserData = (req, res) => {
-    // Get user ID from the token
-    const userId = req.userId;
-
-    // Retrieve user data from the database
-    Users.findByPk(userId, { attributes: ['Username', 'Email'] })
-    .then(user => {
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        // Respond with user data
-        res.status(200).json({ user });
-    })
-    .catch(error => {
-        console.error('Error getting user data:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    });
-};
-exports.UserLogout = (req, res) => {
-    const token = req.header('Authorization').substring(7); // Extract token
-
-    // Blacklist the token
-    authMiddleware.blacklistToken(token);
-
-    res.status(200).json({ message: 'Logout successful' });
-};
-
-// Function to validate email format
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
