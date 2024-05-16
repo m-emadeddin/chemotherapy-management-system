0421b29478d172c630583a1be3ee020092ea7e32
alaa wahba <alaawahbaa13@gmail.com>
2024-05-16 14:45:52 +0300

    fixing promises error

--- server-side/src/controllers/OrderChemotherapy.controllers.js
+++ server-side/src/controllers/OrderChemotherapy.controllers.js
@@ -16,29 +16,30 @@ exports.getRegimens = (req, res, next) => {
       if (!patient) {
         return res.status(404).json({ error: 'Patient Not found' });
       }
-      return patient.getCancerOverview();
-    })
-    .then((Canceroverview) => {
-      if (!Canceroverview) {
-        return res.status(400).json({ error: 'Canceroverview not found' });
-      }
-      return Canceroverview.Cancer_type;
-    })
-    .then((cancerType) => {
-      const cleanedCancerType = removeSpecialCharacters(cancerType);
-      console.log(cleanedCancerType,cancerType);
-      return TreatmentPlanReadOnly.findAll({
-        where: {Cancer_Type: {
-          [Op.like]: `%${cancerType.toLowerCase()}%`
-        }},
-        attributes: ['Plan_Name', 'Plan_ID','number_of_Cycles','number_of_Weeks'],
-      });
-    })
-    .then((regimenName) => {
-      if (!regimenName || regimenName.length == 0) {
-        return res.status(404).json({ error: 'Regimen not found' });
-      }
-      res.status(200).json({ regimenName });
+       patient.getCancerOverview().then((Canceroverview) => {
+        if (!Canceroverview) {
+          return res.status(400).json({ error: 'Canceroverview not found' });
+        }
+        return Canceroverview.Cancer_type;
+      })
+      .then((cancerType) => {
+        const cleanedCancerType = removeSpecialCharacters(cancerType);
+        console.log(cleanedCancerType,cancerType);
+        return TreatmentPlanReadOnly.findAll({
+          where: {Cancer_Type: {
+            [Op.like]: `%${cancerType.toLowerCase()}%`
+          }},
+          attributes: ['Plan_Name', 'Plan_ID','number_of_Cycles','number_of_Weeks'],
+        });
+      })
+      .then((regimenName) => {
+        if (!regimenName || regimenName.length == 0) {
+          return res.status(404).json({ error: 'Regimen not found' });
+        }
+        res.status(200).json({ regimenName });
+      }) .catch((err) => {
+        console.log(err);
+      })
     })
     .catch((err) => {
       console.log(err);
@@ -52,21 +53,20 @@ exports.getChemoMedications = (req, res, next) => {
       if (!regimen) {
         return res.status(404).json({ error: 'Regimen not found' });
       }
-      return regimen.getChemotherapyMedReads();
-    })
-    .then((chemotherapy) => {
-      // fetches the chemotherapy related to certain regimen
-      const chemoMeds = chemotherapy.map((chemoMedication) => ({
-        // fetches one Medication  & iterates over all chemotherapy Medications
-        name: chemoMedication.Medication_Name,
-        dose: chemoMedication.Dose,
-        route: chemoMedication.Route,
-        Instructions: chemoMedication.Instructions,
-      }));
-      return chemoMeds;
-    })
-    .then((chemoMedications) => {
-      res.status(200).json({ chemoMedications });
+       regimen.getChemotherapyMedReads().then((chemotherapy) => {
+        // fetches the chemotherapy related to certain regimen
+        const chemoMeds = chemotherapy.map((chemoMedication) => ({
+          // fetches one Medication  & iterates over all chemotherapy Medications
+          name: chemoMedication.Medication_Name,
+          dose: chemoMedication.Dose,
+          route: chemoMedication.Route,
+          Instructions: chemoMedication.Instructions,
+        }));
+        return chemoMeds;
+      })
+      .then((chemoMedications) => {
+        res.status(200).json({ chemoMedications });
+      })
     })
     .catch((err) => {
       console.log(err);
@@ -81,19 +81,18 @@ exports.getPreMedications = (req, res, next) => {
         console.log(regimen);
         return res.status(404).json({ error: 'Regimen not found' });
       }
-      return regimen.getPremedicationReads();
-    })
-    .then((premedications) => {
-      const preMed = premedications.map((medication) => ({
-        name: medication.Medication_Name,
-        dose: medication.Dose,
-        route: medication.Route,
-        Instructions: medication.Instructions,
-      }));
-      return preMed;
-    })
-    .then((preMedications) => {
-      res.status(200).send({ preMedications });
+       regimen.getPremedicationReads().then((premedications) => {
+        const preMed = premedications.map((medication) => ({
+          name: medication.Medication_Name,
+          dose: medication.Dose,
+          route: medication.Route,
+          Instructions: medication.Instructions,
+        }));
+        return preMed;
+      })
+      .then((preMedications) => {
+        res.status(200).send({ preMedications });
+      })
     })
     .catch((err) => {
       console.log(err);
