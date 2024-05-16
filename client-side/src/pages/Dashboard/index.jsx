e8799741a78ac2a7a2a7afb05bffe28eee3be0a9
m-emadeddin <mohamed.emad12363@gmail.com>
2024-05-17 00:46:25 +0300

    created dashboard as a landing page instead of select patient page

--- client-side/src/pages/Dashboard/index.jsx
+++ client-side/src/pages/Dashboard/index.jsx
@@ -15,107 +15,24 @@ import {
   Img,
 } from "../../components";
 import { useSelectedPatientInfo } from "contexts/SelectedPatientInfoDetails";
+import Sidebar from "components/Sidebar/Sidebar";
 
 export default function Dashboard() {
-  /* Here to get set your needed variables*/
 
-  const [expanded, setExpanded] = useState(true);
 
-  useEffect(() => {}, [expanded]);
 
   return (
     <>
       <Helmet>
-        <title>CMS App</title>
+        <title>Oncology MS</title>
         <meta
           name="description"
           content="Web site created using create-react-app"
         />
       </Helmet>
       <div className="mx-auto flex w-full max-w-[1321px] flex-col gap-[30px] md:p-5 mt-[100px]">
-        <div
-          className={`${
-            expanded ? "w-[5%]" : "w-[19%]"
-          } flex flex-col bg-white-A700 absolute top-[55px] left-0 h-full`}
-        >
-          <div className="w-full flex flex-col items-center gap-2 p-2">
-            {
-              <>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/user-group.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>All Patients</div>}
-                </Button>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/active-patients.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>Active Patients</div>}
-                </Button>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/non-active.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>Non-active Patients</div>}
-                </Button>
-                <Button
-                  className="w-full  flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/high-risk.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>High Risk Patients</div>}
-                </Button>
-              </>
-            }
-          </div>
-
-          <Button
-            onClick={() => {
-              setExpanded(!expanded);
-              console.log(expanded);
-              console.log(1);
-            }}
-          >
-            <div className="bg-white-A700 absolute top-5 rounded-full p-2 right-[-18px] border-gray-100 border border-4">
-              <Img
-                // className={"absolute top-5"}
-                src={`/images/chevron-${expanded ? "right" : "left"}.svg`}
-              ></Img>
-            </div>
-          </Button>
-        </div>
+      
+      <Sidebar/>
 
         <div className="w-[81%] flex flex-col gap-[30px] self-end">
           <PatientInfo />
