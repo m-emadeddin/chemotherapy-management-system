3c1c6062b5584021b941fb35a9da7bf57260e6f2
m-emadeddin <mohamed.emad12363@gmail.com>
2024-05-17 01:44:07 +0300

    refactor sidebar logic and style

--- client-side/src/components/Sidebar/Sidebar.jsx
+++ client-side/src/components/Sidebar/Sidebar.jsx
@@ -5,108 +5,114 @@ import PatientTable from "components/PatientTable";
 import PatientInfo from "components/PatientInfo";
 
 import {
-  Text,
   Button,
-  Heading,
-  CycleDetails,
-  CycleDocument,
-  DocumentChemotherapyCycle,
-  WarningPopUp,
   Img,
 } from "../";
-import { useSelectedPatientInfo } from "contexts/SelectedPatientInfoDetails";
 
 const Sidebar = () => {
-    
   const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {}, [expanded]);
 
   return (
     <div>
-        <div
-          className={`${
-            expanded ? "w-[5%]" : "w-[19%]"
-          } flex flex-col bg-white-A700 absolute top-[55px] left-0 h-full`}
-        >
-      <div className="w-full flex flex-col items-center gap-2 p-2">
-            {
-              <>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/user-group.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>All Patients</div>}
-                </Button>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/active-patients.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>Active Patients</div>}
-                </Button>
-                <Button
-                  className="w-full flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/non-active.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>Non-active Patients</div>}
-                </Button>
-                <Button
-                  className="w-full  flex !justify-start gap-5 hover:bg-blue-100"
-                  size="sm"
-                  shape="round"
-                  leftIcon={
-                    <Img
-                      src="/images/high-risk.svg"
-                      alt="arrow_left"
-                      className="h-[14px] w-[14px]"
-                    />
-                  }
-                >
-                  {!expanded && <div>High Risk Patients</div>}
-                </Button>
-              </>
-            }
-          </div>
+      <div
+        className={`${
+          expanded ? "w-[5%]" : "w-[19%]"
+        } flex flex-col bg-white-A700 absolute top-[55px] left-0 h-full`}
+      >
+        <div className="w-full flex flex-col items-center gap-2 p-2">
+          {
+            <>
+              <Button
+                className="w-full flex !justify-start gap-5 hover:bg-blue-100"
+                size="sm"
+                shape="round"
+                leftIcon={
+                  <Img
+                    src="/images/user-group.svg"
+                    alt="arrow_left"
+                    className="h-[14px] w-[14px]"
+                  />
+                }
+              >
+                {!expanded && <div>All Patients</div>}
+              </Button>
+              <Button
+                className="w-full flex !justify-start gap-5 hover:bg-blue-100"
+                size="sm"
+                shape="round"
+                leftIcon={
+                  <Img
+                    src="/images/active-patients.svg"
+                    alt="arrow_left"
+                    className="h-[14px] w-[14px]"
+                  />
+                }
+              >
+                {!expanded && <div>Active Patients</div>}
+              </Button>
+              <Button
+                className="w-full flex !justify-start gap-5 hover:bg-blue-100"
+                size="sm"
+                shape="round"
+                leftIcon={
+                  <Img
+                    src="/images/non-active.svg"
+                    alt="arrow_left"
+                    className="h-[14px] w-[14px]"
+                  />
+                }
+              >
+                {!expanded && <div>Non-active Patients</div>}
+              </Button>
+              <Button
+                className="w-full  flex !justify-start gap-5 hover:bg-blue-100"
+                size="sm"
+                shape="round"
+                leftIcon={
+                  <Img
+                    src="/images/high-risk.svg"
+                    alt="arrow_left"
+                    className="h-[14px] w-[14px]"
+                  />
+                }
+              >
+                {!expanded && <div>High Risk Patients</div>}
+              </Button>
+              <Button
+                className="w-full  flex !justify-start gap-5 hover:bg-blue-100"
+                size="sm"
+                shape="round"
+                leftIcon={
+                  <Img
+                    src="/images/high-risk.svg"
+                    alt="arrow_left"
+                    className="h-[14px] w-[14px]"
+                  />
+                }
+              >
+                {!expanded && <div>Reports & Schedule</div>}
+              </Button>
+            </>
+          }
+        </div>
 
+        <div className="bg-white-A700 absolute top-5 rounded-full p-2 right-[-18px] border-gray-100 border border-4 hover:bg-blue-100">
           <Button
             onClick={() => {
               setExpanded(!expanded);
             }}
           >
-            <div className="bg-white-A700 absolute top-5 rounded-full p-2 right-[-18px] border-gray-100 border border-4">
-              <Img
-                // className={"absolute top-5"}
-                src={`/images/chevron-${expanded ? "right" : "left"}.svg`}
-              ></Img>
-            </div>
+            <Img
+              // className={"absolute top-5"}
+              src={`/images/chevron-${expanded ? "right" : "left"}.svg`}
+            ></Img>
           </Button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
