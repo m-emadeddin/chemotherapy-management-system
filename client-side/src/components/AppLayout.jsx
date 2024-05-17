d60adbc9d83133b84cd90a9f59c458c611789d62
AhmedAtef <aatef83156@gmail.com>
2024-05-17 08:26:15 +0300

    Add Footer

--- client-side/src/components/AppLayout.jsx
+++ client-side/src/components/AppLayout.jsx
@@ -5,8 +5,12 @@ function AppLayout() {
   return (
     <div className="flex flex-1 w-full flex-col items-center gap-[15px] bg-gray-100 overflow-y-auto">
       {/* header section */}
-      <Header className=" fixed  w-full top-0  flex items-center justify-center self-stretch border-b border-solid border-gray-400 bg-white-A700 p-2 shadow-xs z-50 " />
+      <Header className="fixed w-full top-0 flex items-center justify-center self-stretch border-b border-solid border-gray-400 bg-white-A700 p-2 shadow-xs z-50" />
       <Outlet />
+
+      <footer className="bg-gray-200 text-center py-4 w-full">
+        <p className="text-sm text-gray-600">© 2024 Oncology MS. All rights reserved to Codeless.</p>
+      </footer>
     </div>
   );
 }
