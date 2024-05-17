af799a801f9d99715ad8f082468caaa98fff489e
Gehad Alaa <125475192+Gehad799@users.noreply.github.com>
2024-05-18 02:00:05 +0300

    Update PlansDetails.js

--- client-side/src/contexts/PlansDetails.js
+++ client-side/src/contexts/PlansDetails.js
@@ -47,7 +47,7 @@ export const PlansDetailsProvider = ({ children }) => {
           console.log("Error in fetching PlansDetails", err);
         });
     }
-  }, [selectedPatientInfo, selectedPatientInfo.Patient_ID]);
+  }, [selectedPatientInfo]);
 
   return (
     <PlanDetailsContext.Provider
