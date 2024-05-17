5bdd6e8a094ebc7964f4c8fcef9c21fe46db6e1c
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2024-05-17 12:07:19 +0300

    adding get review chemotherapy and pysician note in models

--- server-side/src/controllers/ReviewChemotherapy.controllers.js
+++ server-side/src/controllers/ReviewChemotherapy.controllers.js
@@ -19,12 +19,14 @@ exports.reviewChemotheraby = async (req, res, next) => {
       PreMedications,
       ChemotherapyMedications,
       Start_Date,
+      physician_note
     } = req.body;
 
     const treatmentPlan = await db.TreatmentPlans.create({
       Plan_Name,
       number_of_Weeks,
       number_of_Cycles,
+      physician_note,
       patientPatientID: patient.Patient_ID,
     });
     console.log("True Treatment");
@@ -109,6 +111,78 @@ exports.reviewChemotheraby = async (req, res, next) => {
     res.json({ message: "Data inserted successfully" });
   } catch (error) {
     console.error("Error inserting data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+
+exports.getTreatmentPlan = async (req, res, next) => {
+  try {
+    const { patientId } = req.params;
+    const patient = await db.Patients.findByPk(patientId);
+    if (!patient) {
+      return res.status(404).json({ error: "Patient not found" });
+    }
+
+    const treatmentPlan = await patient.getTreatmentPlan();
+    if (!treatmentPlan) {
+      return res.status(404).json({ error: "Treatment plan not found" });
+    }
+
+    const cycles = await treatmentPlan.getCycles({ limit: 1 });
+    const premedications = await Promise.all(
+      cycles.map(async (cycle) => {
+        return await cycle.getPremedications({
+          attributes: [
+            "Medication_Name",
+            "Dose",
+            "Route",
+            "Instructions",
+          ],
+        });
+      })
+    );
+    const chemotherapyMedications = await Promise.all(
+      cycles.map(async (cycle) => {
+        return await cycle.getChemotherapyMedications({
+          attributes: [
+            "Medication_Name",
+            "Dose",
+            "Route",
+            "Instructions",
+            "Dosage_Reduction",
+          ],
+        });
+      })
+    );
+
+    const formattedResponse = {
+      Plan_Name: treatmentPlan.Plan_Name,
+      number_of_Weeks: treatmentPlan.number_of_Weeks,
+      number_of_Cycles: treatmentPlan.number_of_Cycles,
+      physician_note: treatmentPlan.physician_note,
+
+      Start_Date: cycles[0].Start_Date.toISOString().split("T")[0],
+      PreMedications: premedications.flat().map((medication) => {
+        const { Medication_Name, Dose, Route, Instructions } = medication.dataValues;
+        return { Medication_Name, Dose, Route, Instructions };
+      }),
+      ChemotherapyMedications: chemotherapyMedications.flat().map((medication) => {
+        const { Medication_Name, Dose, Route, Instructions, Dosage_Reduction } = medication.dataValues;
+        return {
+          Medication_Name,
+          Dose,
+          Route,
+          Instructions,
+          Dosage_Reduction,
+        };
+      }),
+    };
+
+    res.json(formattedResponse);
+  } catch (error) {
+    console.error("Error retrieving data:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
